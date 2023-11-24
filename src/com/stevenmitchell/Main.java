59c99b32882ba6e08a17ce1e52018fa81e785245
Steven Mitchell <stemitchlfc1995@gmail.com>
2023-11-24 11:53:34 +0000

    single use method with calls for different time periods

--- src/com/stevenmitchell/Main.java
+++ src/com/stevenmitchell/Main.java
@@ -3,12 +3,25 @@ package com.stevenmitchell;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int total = 0;
-        for(int i = 1; i<=365; i++){
-            total += i;
-            System.out.println(i + " " +total);
-        }
-        System.out.println(total);
+	    int weekAmount = dollarADay(7);
+        System.out.println(weekAmount);
+        int monthAmount = dollarADay(31);
+        System.out.println(monthAmount);
+        int halfYearAmount = dollarADay(180);
+        System.out.println(halfYearAmount);
+        int yearAmount = dollarADay(365);
+        System.out.println(yearAmount);
+        int leapYear = dollarADay(366);
+        System.out.println(leapYear);
     }
+    public static int dollarADay(int days){
+        int total = 0;
+        for(int i = 1; i<=days; i++){
+            total += i;
+        }
+        return total;
+    }
+
+
+
 }
